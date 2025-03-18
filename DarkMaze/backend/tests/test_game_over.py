57205766901e95d7a1cbcd3a3b3843a081ce0763
ian293382 <moshou5571228@gmail.com>
2025-03-19 00:46:51 +0800

    feat:implement test game over state

--- DarkMaze/backend/tests/test_game_over.py
+++ DarkMaze/backend/tests/test_game_over.py
@@ -1,10 +1,11 @@
 from judge_code import game_over
 
 def test_game_over_0():
-    ...
+    assert game_over(0) == True
 
 def test_game_over_666():
-    ...
+    assert game_over(666) == True
 
 def test_game_over_other():
-    ...
+    # i.e health = 3 give other number
+    assert game_over(3) == False
