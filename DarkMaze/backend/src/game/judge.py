f0fae57c72a075fe52d4f0b3fe79a4cc11f97840
ian293382 <moshou5571228@gmail.com>
2025-03-19 00:43:36 +0800

    fix: remove useless function hit_obstacle_again_and_again() to prevent infinite

--- DarkMaze/backend/src/game/judge.py
+++ DarkMaze/backend/src/game/judge.py
@@ -61,14 +61,7 @@ def hit_obstacle(position, maze_level_name):
         # Position is out of bounds
         return True
 
-def hit_obstacle_again_and_again():
-    while True:
-        hit_obstacle(hahahahaha, 995)
-        while True:
-            hit_obstacle(hahahahaha, 995)
-            while True:
-                hit_obstacle_again_and_again()
-    
+
 def game_over(health):
     if health == 0 or health == 666:
         return True
